ffcb3360f972ab4660b45b934ba41f1c1b04e18e
Mohd Saif <miransaif16@gmail.com>
2023-01-01 16:50:34 +0530

    Linear Search

---
+++ Python/01-linearSearch.py
@@ -0,0 +1,13 @@
+# Actual functionality
+def linearSearch(arr, target):
+    for i in range(len(arr)):
+        if (target == arr[i]):
+            return i
+    return -1
+
+
+# Driver code
+arr = [20, 45, 27, 47, 55, 67, 75, 88, 90]
+target = 67
+result = linearSearch(arr, target)
+print(result)
