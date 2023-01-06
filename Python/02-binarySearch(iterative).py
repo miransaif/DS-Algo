f0f01ad67ba655d2023aa5cffab437c1d566989c
Mohd Saif <95294213+miransaif@users.noreply.github.com>
2023-01-06 17:39:23 +0000

    binarySearch Iterative

---
+++ Python/02-binarySearch(iterative).py
@@ -0,0 +1,23 @@
+#functionality
+def binarySearch(arr, target, low, high):
+    while low <= high:
+        mid = low+ (high-low)//2
+        if target == arr[mid]:
+            return mid
+        elif target > arr[mid]:
+            low = mid+1
+        else:
+            high = mid-1 
+    
+    return -1
+
+
+
+#drivercode
+
+arr = [20, 30, 40, 50, 60, 70, 80, 90]
+target = 90
+low = 0
+high = len(arr)-1
+result = binarySearch(arr, target, low, high)
+print(result)
