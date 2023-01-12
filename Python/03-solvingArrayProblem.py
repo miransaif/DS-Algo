a67f196797477003f7937103e14568cdb2aa8661
Mohd Saif <95294213+miransaif@users.noreply.github.com>
2023-01-12 20:20:43 +0000

    Array is sorted and find 2 numbers whose sum is 210 from arrary

---
+++ Python/03-solvingArrayProblem.py
@@ -0,0 +1,21 @@
+#Problem Statement : Array is sorted and find 2 numbers whose sum is 210 from arrary
+
+# funtionality
+def searchOperation(arr, low, high, target):
+    while low <= high:
+        if arr[low]+arr[high] == target:
+            return(low, high)
+        elif target < arr[low]+arr[high]:
+            high = high-1
+        else:
+            low = low+1
+
+
+# driver code
+
+arr = [20, 40, 60, 80, 90, 120, 240]
+low = 0
+high = len(arr)-1
+target = 210
+result = searchOperation(arr, low, high, target)
+print(result)
